536da07bf251ee903e172b18c21798fe30cf6896
Finlayalpha <fincoulson@outlook.com>
2026-07-05 17:37:40 +0100

    Add: last-stone-weight - submission-7

---
+++ Data Structures & Algorithms/last-stone-weight/submission-7.py
@@ -0,0 +1,20 @@
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        
+        while len(stones) > 1:
+            stones.sort()
+            big, small = stones[-1], stones[-2]
+            if big == small:
+                stones.remove(big)
+                stones.remove(small)
+            elif big > small:
+                remainder = big - small
+                stones[-1] = remainder
+                stones.remove(small)
+
+        if len(stones) == 1:
+            return stones[0]
+        else:
+            return 0
+            
+            
