d9922778eef3c97c4b99515cdd56d5fedae33011
Finlayalpha <fincoulson@outlook.com>
2026-07-16 10:16:03 +0100

    Add: balanced-binary-tree - submission-0

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-0.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+
+        balanced = True
+
+        def height(node):
+            nonlocal balanced
+
+            if node is None:
+                return 0
+
+            left_height = height(node.left)
+            right_height = height(node.right)
+
+            if abs(left_height - right_height) > 1:
+                balanced = False
+
+            return 1 + max(left_height, right_height)
+
+        height(root)
+        return balanced
